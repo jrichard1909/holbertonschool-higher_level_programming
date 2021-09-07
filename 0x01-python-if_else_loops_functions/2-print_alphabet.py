1092310f777d3ccd91834fb19fa21a30211f166b
jrichard1909 <jrichard1909@gmail.com>
2021-09-07 16:15:45 -0700

    2-print_alphabet.py file updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for n in range(ord('a'), ord('z') + 1):
-    print('{}' .format(chr(n)), end='')    
+for n in range(ord('a'), ord('z')+1):
+    print('{}'.format(chr(n)), end='')    
