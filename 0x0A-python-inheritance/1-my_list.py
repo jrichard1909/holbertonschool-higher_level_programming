39b7db2834a7be1ef91f5eb80d2318065626e06a
jrichard1909 <jrichard1909@gmail.com>
2021-10-11 20:03:51 -0700

    1-my_list.py and tests/1-my_list.txt created

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""MyList
+"""
+
+
+class MyList(list):
+    """inherits 
+    """
+
+    def print_sorted(self):
+        """Prints sorted list
+        """
+
+        print(sorted(self))
