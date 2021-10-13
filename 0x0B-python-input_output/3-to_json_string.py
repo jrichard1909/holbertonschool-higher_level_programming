789bcf992251b3bfd838a903ca4342639ae334c5
jrichard1909 <jrichard1909@gmail.com>
2021-10-12 20:16:02 -0700

    4-from_json_string.py  function createdx

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    To JSON string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation
+    of an object (string)
+    """
+    
+    return json.dumps(my_obj)
