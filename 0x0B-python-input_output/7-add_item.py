36dec203601e13ccc7eb9927ab7d85babc6ff61e
jrichard1909 <jrichard1909@gmail.com>
2021-10-12 21:22:00 -0700

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Load, add, save
+"""
+import json, sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+_list = load_from_json_file(filename)
+
+for i in range(1, len(sys.argv)):
+    _list.append(sys.argv[i])
+
+save_to_json_file(_list, filename)
+
+
