950e206248a989231407502f7b07fa45ff9d457d
jrichard1909 <jrichard1909@gmail.com>
2021-10-12 21:36:59 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 
-_list = load_from_json_file(filename)
+try:
+    _list = load_from_json_file(filename)
+except:
+    _list = []
 
 for i in range(1, len(sys.argv)):
     _list.append(sys.argv[i])
