05c5e78882851030b48442fce7265d9bdb5403f1
jrichard1909 <jrichard1909@gmail.com>
2021-10-16 15:05:01 -0700

    10-square.py created

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+   Square #1
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Rectangle that inherits from BaseGeometry
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
