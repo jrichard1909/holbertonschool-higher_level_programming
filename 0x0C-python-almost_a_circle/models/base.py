272dce026a0aed8e42dbe2d84f03971e6aeb4419
unknown <jrichard1909@gmail.com>
2021-10-18 23:08:43 -0500

    base class fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,12 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        static method that returns the JSON string
+        representation of list_dictionaries
+        """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
