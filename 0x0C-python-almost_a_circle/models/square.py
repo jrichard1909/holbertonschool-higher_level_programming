c6f0978a66abf5a7831cffc89483f4bdd127a8e7
unknown <jrichard1909@gmail.com>
2021-10-18 23:14:02 -0500

    add coments to Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,22 +11,27 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """constructor init"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """overloading __str__ method"""
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """Getter"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """public method that assigns attributes"""
         if len(args) != 0:
             if len(args) > 0:
                 self.id = args[0]
