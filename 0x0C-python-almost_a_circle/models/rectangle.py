cef399c3d82bdf4167c7ef851875429211808b88
unknown <jrichard1909@gmail.com>
2021-10-18 22:30:40 -0500

    Rectangle class fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor init"""
         super().__init__(id)
         self.width = width
         self.height = height
