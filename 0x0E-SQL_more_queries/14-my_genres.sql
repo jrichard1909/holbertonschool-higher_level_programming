4aac19691f6e4ce0fd4134c1b652b3e049f89dcb
jrichard1909 <jrichard1909@gmail.com>
2021-11-18 16:40:48 -0800

    14-my_genres.sql updated. order by name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ SELECT tv_genres.name
 FROM (tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id)
 INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter';
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
