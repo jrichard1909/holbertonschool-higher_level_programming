4cc8221a70d1c8032ce6d83f2d7354655d6c62fc
jrichard1909 <jrichard1909@gmail.com>
2021-11-18 17:08:20 -0800

    2-create_read_user.sql updated

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,6 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd;';
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT
-ON hbtn_0d_2.*
+ON 'hbtn_0d_2'.*
 TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
