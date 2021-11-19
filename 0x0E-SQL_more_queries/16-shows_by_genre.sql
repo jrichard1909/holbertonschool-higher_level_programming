59122c8bcc80805e8c1dba9235400b432b4c1aae
jrichard1909 <jrichard1909@gmail.com>
2021-11-18 16:38:38 -0800

    16-shows_by_genre.sql updated. order by genre name

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title, tv_genres.name
 FROM (tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id)
 RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title
+ORDER BY tv_shows.title, tv_genres.name
