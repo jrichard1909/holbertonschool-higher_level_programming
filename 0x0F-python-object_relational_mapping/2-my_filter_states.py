fac57ba63af1f79e3b66634d67081e2750e04557
jrichard1909 <jrichard1909@gmail.com>
2022-08-06 15:43:25 -0500

    Fix task 2 - Filter states by user input. Fix pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '" + sys.argv[4] + "' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name = '"
+                + sys.argv[4] + "' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
