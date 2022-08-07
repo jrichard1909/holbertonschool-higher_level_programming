980516d5aa594a4f606b702843beb4949ad3c38f
jrichard1909 <jrichard1909@gmail.com>
2022-08-07 14:08:10 -0500

    Add task 13 - Delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that deletes all State objects
+with a name containing the letter a from
+the database hbtn_0e_6_usa
+"""
+from audioop import getsample
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    session.query(State).filter(State.name.like('%a%')). \
+        delete(synchronize_session='fetch')
+    session.commit()
+
+    session.close()
