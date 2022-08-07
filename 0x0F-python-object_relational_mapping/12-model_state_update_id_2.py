4d14d8dfeb531980ef0ff2e7d9af822f4dd43909
jrichard1909 <jrichard1909@gmail.com>
2022-08-07 13:51:02 -0500

    Add task 12 - Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that changes the name of a State object
+from the database hbtn_0e_6_usa
+"""
+from audioop import getsample
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    getState = session.query(State).get(2)
+    getState.name = 'New Mexico'
+    session.add(getState)
+    session.commit()
+
+    session.close()
