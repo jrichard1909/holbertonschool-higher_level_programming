f7240ac8fcbe7b32e8c88ff52e07997add04a4f9
jrichard1909 <jrichard1909@gmail.com>
2022-08-06 22:30:31 -0500

    Add task 3 - SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" all states with a name starting with N (upper N) from the database """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name = %s ORDER BY \
+                id""", [sys.argv[4]])
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
