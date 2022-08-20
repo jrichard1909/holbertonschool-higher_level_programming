c10e7cacf73d75900acc4dd782621cae26ff97a0
jrichard1909 <jrichard1909@gmail.com>
2022-08-20 01:25:38 -0500

    add task 8 Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends
+a request to the URL and displays the body
+of the response
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    q = ''
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+
+    r = requests.post('http://0.0.0.0:5000/search_user', {'q': q})
+    try:
+        data = r.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("Not result")
+    except Exception:
+        print("Not a valid JSON")
