dd81292be1ede4594e894d14a6ff1a5c2d846b6e
jrichard1909 <jrichard1909@gmail.com>
2022-08-19 21:46:36 -0500

    fix task 0 for pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@ import urllib.request
 req = "https://intranet.hbtn.io/status"
 with urllib.request.urlopen(req) as response:
     data = response.read()
-   
-print ("Body response:")
-print ("\t- type: {}".format(type(data)))
-print ("\t- content: {}".format(data))
-print ("\t- uft8 content: {}".format(data.decode()))
+
+print("Body response:")
+print("\t- type: {}".format(type(data)))
+print("\t- content: {}".format(data))
+print("\t- uft8 content: {}".format(data.decode()))
