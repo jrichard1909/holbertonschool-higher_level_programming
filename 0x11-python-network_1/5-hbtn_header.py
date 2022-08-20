abc6f6bb45e751d31881a17b71cf8e15644ea6b0
jrichard1909 <jrichard1909@gmail.com>
2022-08-20 00:29:42 -0500

    fix task 5 updated entry point

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,8 +5,9 @@ a request to the URL and displays the
 value of the variable X-Request-Id in the
 response header
 """
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-r = requests.get(sys.argv[1])
-print(r.headers.get('X-Request-Id'))
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
