ed50a697d5fd2ae1121895df9635c92cbe14f5c3
jrichard1909 <jrichard1909@gmail.com>
2022-12-15 01:54:56 -0500

    add task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+request('https://swapi-api.hbtn.io/api/films/' + id + '/', function (error, response, body) {
+  if (error == null) {
+    const json = JSON.parse(body);
+    console.log(json.title);
+  }
+});
