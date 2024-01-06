4a4add6c855825615eec936955f60ca8699efbbd
John Chavez <68090271+jrichard1909@users.noreply.github.com>
2024-01-06 14:33:03 -0500

    Delete 0x06-python-classes directory

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""module 0-square.py"""
-
-
-class Square:
-    "class Square that defines a square by"
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""module 1-square.py"""
-
-
-class Square:
-    """class Square that defines a square by"""
-    def __init__(self, size):
-        self.__size = size
