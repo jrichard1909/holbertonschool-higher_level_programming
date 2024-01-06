1cc96da232361ec334a56d520dea8d34848ad31f
John Chavez <68090271+jrichard1909@users.noreply.github.com>
2024-01-06 14:33:48 -0500

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++
@@ -1,7 +0,0 @@
--- Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)
--- script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++
@@ -1,8 +0,0 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql)
--- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
