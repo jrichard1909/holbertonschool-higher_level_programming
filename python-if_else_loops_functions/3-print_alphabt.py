f5afb5d755e5f6ef3b90833ead14341871187c04
jrichard1909 <jrichard1909@gmail.com>
2024-01-21 17:41:29 -0500

    fix 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord("a"), ord("z") + 1):
     if chr(a) != "q" and chr(a) != "e":
-        print("{:s}".format(chr(a)), end="")
+        print(f"{chr(a)}", end="")
