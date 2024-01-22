5e88bf1182af483fcf3202583492a7f8f9eb7cfc
jrichard1909 <jrichard1909@gmail.com>
2024-01-21 20:11:40 -0500

    fix over indented and missing whitespace errors

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-a = 89
-b = 10
+a = 10
+b = 89
 a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     total = len(sentence)
     char = sentence[0] if total > 0 else "None"
     new = total, char
-    return(new)
+    return (new)
